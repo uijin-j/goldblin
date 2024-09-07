983d6202a7fd21eb6506d6802f5719030e983494
uijin-j <uijin0531@gmail.com>
2024-09-07 18:46:12 +0900

    test: GoldblinXXXApplication 테스트 코드 추가 (#1)

---
+++ goldblin-auth/src/test/java/goldblin/auth/GoldblinAuthApplicationTest.java
@@ -0,0 +1,12 @@
+package goldblin.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldblinAuthApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}

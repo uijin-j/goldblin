ebaa3e97e5901f3308c57a97ef2b0119ed6755ed
uijin-j <uijin0531@gmail.com>
2024-09-09 01:48:29 +0900

    feat: 로그인 API 엔트포인트 추가 및 문서화 (#5)

--- goldblin-auth/src/main/java/goldblin/auth/presentation/docs/AuthApiControllerDoc.java
+++ goldblin-auth/src/main/java/goldblin/auth/presentation/docs/AuthApiControllerDoc.java
@@ -1,6 +1,8 @@
 package goldblin.auth.presentation.docs;
 
+import goldblin.auth.dto.request.LoginReq;
 import goldblin.auth.dto.request.SignUpReq;
+import goldblin.auth.dto.response.LoginRes;
 import goldblin.common.api.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,4 +12,7 @@ public interface AuthApiControllerDoc {
 
 	@Operation(summary = "회원가입")
 	ApiResponse<Void> signup(SignUpReq request);
+
+	@Operation(summary = "로그인")
+	ApiResponse<LoginRes> login(LoginReq request);
 }

12f5413c46a896841c65dc28f9c67e492dfd15b9
uijin-j <uijin0531@gmail.com>
2024-09-08 16:36:39 +0900

    feat: Auth 관련 에러/응답 메시지를 관리하는 AuthMessages 생성 (#3)

---
+++ goldblin-auth/src/main/java/goldblin/auth/constants/AuthMessages.java
@@ -0,0 +1,14 @@
+package goldblin.auth.constants;
+
+public class AuthMessages {
+
+	/**
+	 * 400 번대 에러 메시지
+	 */
+	public static final String SHORT_PASSWORD = "비밀번호는 8자 이상이어야 합니다.";
+	public static final String INVALID_PASSWORD_FORMAT = "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 합니다.";
+	public static final String BLANK_USERNAME = "아이디는 필수값입니다.";
+	public static final String TOO_LONG_USERNAME = "아이디는 50자 이하여야 합니다.";
+	public static final String BLANK_PASSWORD = "비밀번호는 필수값입니다.";
+	public static final String DUPLICATE_USERNAME = "이미 사용중인 아이디입니다.";
+}

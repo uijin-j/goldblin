9e9b66b2805aa4b475448611a801661e13765bd7
uijin-j <uijin0531@gmail.com>
2024-09-08 18:32:41 +0900

    chore: 스웨거 설정 추가 (#3)

---
+++ goldblin-auth/src/main/java/goldblin/auth/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package goldblin.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("GOLDBLIN AUTH API")
+			.description("금도깨비 AUTH API 명세서 ✨")
+			.version("v1");
+	}
+}

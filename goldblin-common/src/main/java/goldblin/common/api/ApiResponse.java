d0c29c68d8486b4d64b1bf450e84ee43e2918860
uijin-j <uijin0531@gmail.com>
2024-09-08 18:34:10 +0900

    docs: ApiResponse 문서화 (#3)

--- goldblin-common/src/main/java/goldblin/common/api/ApiResponse.java
+++ goldblin-common/src/main/java/goldblin/common/api/ApiResponse.java
@@ -4,13 +4,22 @@ import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.HttpStatus;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "API 응답")
 public class ApiResponse<T> {
+	@Schema(description = "상태 코드", example = "200")
 	private int code;
+
+	@Schema(description = "상태", example = "OK")
 	private HttpStatus status;
+
+	@Schema(description = "메시지", example = "요청하신 작업을 완료했습니다.")
 	private String message;
+
+	@Schema(description = "응답 데이터")
 	private T data;
 
 	private ApiResponse(HttpStatus status, String message, T data) {

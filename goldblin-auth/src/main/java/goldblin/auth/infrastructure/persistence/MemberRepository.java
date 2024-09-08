458a671aad089c3aa043a91db7c730286d0940bc
uijin-j <uijin0531@gmail.com>
2024-09-09 01:47:12 +0900

    feat: findByUsername() 메서드 추가 (#5)

--- goldblin-auth/src/main/java/goldblin/auth/infrastructure/persistence/MemberRepository.java
+++ goldblin-auth/src/main/java/goldblin/auth/infrastructure/persistence/MemberRepository.java
@@ -1,9 +1,13 @@
 package goldblin.auth.infrastructure.persistence;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import goldblin.auth.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByUsername(String username);
+
+	Optional<Member> findByUsername(String username);
 }

bce72e8b9c265442fd16034314019b865dcee93a
uijin-j <uijin0531@gmail.com>
2024-09-08 16:38:46 +0900

    feat: 회원가입 비즈니스 로직 구현 (#3)

---
+++ goldblin-auth/src/main/java/goldblin/auth/business/AuthService.java
@@ -0,0 +1,34 @@
+package goldblin.auth.business;
+
+import static goldblin.auth.constants.AuthMessages.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import goldblin.auth.domain.Member;
+import goldblin.auth.domain.service.PasswordManager;
+import goldblin.auth.dto.request.SignUpReq;
+import goldblin.auth.persistence.MemberRepository;
+import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AuthService {
+	private final MemberRepository memberRepository;
+	private final PasswordManager passwordManager;
+
+	@Transactional
+	public Long signup(SignUpReq request) {
+		validateUsernameUnique(request.username());
+		Member member = Member.signup(request.username(), request.password(), passwordManager);
+		return memberRepository.save(member).getId();
+	}
+
+	private void validateUsernameUnique(String username) {
+		if (memberRepository.existsByUsername(username)) {
+			throw new EntityExistsException(DUPLICATE_USERNAME);
+		}
+	}
+}

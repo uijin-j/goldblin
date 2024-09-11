36a01508d63e5ee478bc56d1f5023911225a9367
uijin-j <uijin0531@gmail.com>
2024-09-11 22:02:48 +0900

    rename: OrderCreateReq → OrderPlaceReq 변경 (#7)

--- goldblin-order/src/main/java/goldblin/order/dto/req/OrderPlaceReq.java
+++ goldblin-order/src/main/java/goldblin/order/dto/req/OrderPlaceReq.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record OrderCreateReq(
+public record OrderPlaceReq(
 	@NotBlank(message = BLANK_ORDER_TYPE)
 	String orderType,
 	@NotNull(message = NULL_PRODUCT)

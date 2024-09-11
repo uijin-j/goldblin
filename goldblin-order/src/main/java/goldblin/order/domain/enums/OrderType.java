648f44c623c9da5c4e8153bad7c71bb1ec16d58f
uijin-j <uijin0531@gmail.com>
2024-09-11 21:51:17 +0900

    feat: OrderType 정적 팩터리 메서드 추가 (#7)

--- goldblin-order/src/main/java/goldblin/order/domain/enums/OrderType.java
+++ goldblin-order/src/main/java/goldblin/order/domain/enums/OrderType.java
@@ -1,9 +1,25 @@
 package goldblin.order.domain.enums;
 
+import static goldblin.order.constants.OrderMessages.*;
+import static java.util.Objects.*;
+
+import java.util.Arrays;
+
 public enum OrderType {
 	SELL("판매"),
 	BUY("구매");
 
 	OrderType(String description) {
+	}
+
+	public static OrderType of(String value) {
+		if (isNull(value)) {
+			throw new IllegalArgumentException(BLANK_ORDER_TYPE);
+		}
+
+		return Arrays.stream(OrderType.values())
+			.filter(status -> value.toUpperCase().equals(status.name()))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException(INVALID_ORDER_TYPE));
 	}
 }

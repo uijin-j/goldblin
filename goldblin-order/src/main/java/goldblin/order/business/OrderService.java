d384499a0c879b0411187d9387fa9686131c4999
uijin-j <uijin0531@gmail.com>
2024-09-11 21:59:06 +0900

    feat: 주문 생성 비즈니스 로직 구현 (#7)

---
+++ goldblin-order/src/main/java/goldblin/order/business/OrderService.java
@@ -0,0 +1,61 @@
+package goldblin.order.business;
+
+import static goldblin.order.constants.OrderMessages.*;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import goldblin.order.domain.Order;
+import goldblin.order.domain.Product;
+import goldblin.order.domain.service.OrderNumberGenerator;
+import goldblin.order.dto.req.OrderCreateReq;
+import goldblin.order.infrastructure.persistence.OrderRepository;
+import goldblin.order.infrastructure.persistence.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+	private final OrderRepository orderRepository;
+	private final ProductRepository productRepository;
+	private final OrderNumberGenerator orderNumberGenerator;
+
+	@Transactional
+	public void order(OrderCreateReq request) {
+		// TODO: gRPC 통신으로 유저 정보 받아오기
+		Long memberId = 1L;
+
+		// 주문일자 생성 (현재시간)
+		LocalDate orderedAt = LocalDate.now();
+
+		// 주문번호 생성
+		String orderNumber = orderNumberGenerator.generate(request.orderType(), orderedAt);
+
+		// 상품 조회
+		Product product = getProduct(request);
+
+		// 주문 생성
+		Order order = Order.create(
+			orderNumber,
+			request.orderType(),
+			memberId,
+			product,
+			request.quantity(),
+			request.city(),
+			request.street(),
+			request.zipcode(),
+			orderedAt
+		);
+
+		orderRepository.save(order);
+	}
+
+	private Product getProduct(OrderCreateReq request) {
+		return productRepository.findById(request.productId())
+			.orElseThrow(() -> new EntityNotFoundException(INVALID_PRODUCT));
+	}
+}

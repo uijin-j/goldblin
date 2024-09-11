37b68055bd541426f9cce737edb5ccd618369d77
uijin-j <uijin0531@gmail.com>
2024-09-11 21:58:52 +0900

    feat: 주문 요청 DTO 생성 (#7)

---
+++ goldblin-order/src/main/java/goldblin/order/dto/req/OrderCreateReq.java
@@ -0,0 +1,24 @@
+package goldblin.order.dto.req;
+
+import static goldblin.order.constants.OrderMessages.*;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record OrderCreateReq(
+	@NotBlank(message = BLANK_ORDER_TYPE)
+	String orderType,
+	@NotNull(message = NULL_PRODUCT)
+	Long productId,
+	@NotNull(message = NULL_QUANTITY)
+	BigDecimal quantity,
+	@NotBlank(message = BLANK_ADDRESS)
+	String city,
+	@NotBlank(message = BLANK_ADDRESS)
+	String street,
+	@NotBlank(message = BLANK_ADDRESS)
+	String zipcode
+) {
+}
